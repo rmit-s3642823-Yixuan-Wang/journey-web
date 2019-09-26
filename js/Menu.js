7dec7463eb789722becef062f395a8af5cce0bde
rmit-s3629412-QI-SUN <42260597+rmit-s3629412-QI-SUN@users.noreply.github.com>
2019-09-26 15:31:49 +1000

    old nav-bar

--- js/Menu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-    document.getElementById("main").style.marginLeft = "100px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}
